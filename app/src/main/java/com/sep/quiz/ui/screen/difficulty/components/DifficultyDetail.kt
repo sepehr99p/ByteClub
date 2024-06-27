3c0875d38312a303e8bae6f3eebe261367e401b3
Sep <sepehr.pourekrami@gmail.com>
2024-06-27 16:53:41 +0330

    test dragging component for difficultyDetails

--- app/src/main/java/com/sep/quiz/ui/screen/difficulty/components/DifficultyDetail.kt
+++ app/src/main/java/com/sep/quiz/ui/screen/difficulty/components/DifficultyDetail.kt
@@ -1,25 +1,46 @@
 package com.sep.quiz.ui.screen.difficulty.components
 
+import android.content.res.Resources.getSystem
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sep.quiz.domain.entiry.CategoryInfo
 import com.sep.quiz.domain.entiry.QuestionDifficulty
 import com.sep.quiz.ui.systemDesign.theme.Regular_14
 import com.sep.quiz.ui.systemDesign.theme.dimen.corner_8
 import com.sep.quiz.ui.systemDesign.theme.dimen.padding_8
+import kotlin.math.roundToInt
+
 
 @Composable
 internal fun DifficultyDetail(
@@ -48,8 +69,61 @@ internal fun DifficultyDetail(
             type = QuestionDifficulty.HARD,
             onClick = onClick
         )
+        DraggableNumberSelector()
+
     }
 }
+
+@Composable
+fun DraggableNumberSelector(modifier: Modifier = Modifier) {
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var number by remember { mutableIntStateOf(0) }
+    var maxWidth by remember { mutableFloatStateOf(0f) }
+    val maxNumber = 100
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background)
+            .padding(padding_8)
+            .onGloballyPositioned { coordinates ->
+                maxWidth = coordinates.size.width.toFloat()
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp)
+                .background(MaterialTheme.colorScheme.secondary)
+        )
+
+
+
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), 0) }
+                .clip(CircleShape)
+                .size(40.dp)
+                .background(MaterialTheme.colorScheme.secondary)
+                .draggable(
+                    orientation = Orientation.Horizontal,
+                    state = rememberDraggableState { delta ->
+                        offsetX = (offsetX + delta) //.coerceIn(0f, maxWidth - 40.dp.value)
+                        number = ((offsetX / (maxWidth - (40.dp.value.toInt().px))) * maxNumber).roundToInt() + (maxNumber / 2)
+                    }
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = number.toString(),
+                color = Color.White,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
+
+val Int.px: Int get() = (this * getSystem().displayMetrics.density).toInt()
 
 @Composable
 private fun DifficultyComponent(
