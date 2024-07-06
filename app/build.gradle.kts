9ab5f291decf4937b761f0e43ab13b9fb758c69b
Sep <sepehr.pourekrami@gmail.com>
2024-07-06 17:10:14 +0330

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sep.quiz"
         minSdk = 26
         targetSdk = 34
-        versionCode = 100
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
