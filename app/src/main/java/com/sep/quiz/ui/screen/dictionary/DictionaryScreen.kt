891cd79fa70fbe0eaf4e1065693f59d666544ee8
Sep <sepehr.pourekrami@gmail.com>
2024-07-16 18:50:05 +0330

    basic ui impl for dictionary

--- app/src/main/java/com/sep/quiz/ui/screen/dictionary/DictionaryScreen.kt
+++ app/src/main/java/com/sep/quiz/ui/screen/dictionary/DictionaryScreen.kt
@@ -1,20 +1,64 @@
 package com.sep.quiz.ui.screen.dictionary
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sep.quiz.ui.designSystem.components.ErrorComponent
+import com.sep.quiz.ui.designSystem.components.LoadingComponent
+import com.sep.quiz.ui.utils.UiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DictionaryScreen(
     modifier: Modifier = Modifier,
     viewModel: DictionaryViewModel = hiltViewModel()
 ) {
-    Column(modifier = modifier) {
-        Text(text = "Dictionary", color = MaterialTheme.colorScheme.onPrimary)
+    val searchWord = remember { mutableStateOf("") }
+    val wordDefinition = viewModel.wordDefinition.collectAsState()
+    Column(modifier = modifier.fillMaxSize()) {
+        SearchBar(
+            modifier = Modifier.fillMaxSize(),
+            query = searchWord.value,
+            onQueryChange = { searchWord.value = it },
+            onSearch = { viewModel.fetchWord(searchWord.value) },
+            active = true,
+            onActiveChange = {}
+        ) {
+            when (wordDefinition.value) {
+                is UiState.Failed -> {
+                    ErrorComponent {
+
+                    }
+                }
+
+                is UiState.Initialize -> {
+
+                }
+
+                is UiState.Loading -> {
+                    LoadingComponent()
+                }
+
+                is UiState.Success -> {
+                    Column {
+                        Text(
+                            text = (wordDefinition.value as UiState.Success).data.definition,
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
