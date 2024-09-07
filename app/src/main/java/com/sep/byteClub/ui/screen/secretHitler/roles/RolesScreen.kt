94ee61cc81e961542064e2280d15035d25e2768b
Sep <sepehr.pourekrami@gmail.com>
2024-09-07 16:58:06 +0330

    roles screen ui updated

--- app/src/main/java/com/sep/byteClub/ui/screen/secretHitler/roles/RolesScreen.kt
+++ app/src/main/java/com/sep/byteClub/ui/screen/secretHitler/roles/RolesScreen.kt
@@ -1,13 +1,54 @@
 package com.sep.byteClub.ui.screen.secretHitler.roles
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.patrykandpatrick.vico.core.cartesian.marker.ColumnCartesianLayerMarkerTarget
 import com.sep.byteClub.ui.designSystem.theme.ByteClubTheme
 
 @Composable
-fun RolesScreen(modifier: Modifier = Modifier) {
+fun RolesScreen(
+    modifier: Modifier = Modifier,
+    viewModel: RolesViewModel = hiltViewModel()
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
+    }
+}
+
+@Composable
+private fun HiddenRoleComponent(modifier: Modifier = Modifier) {
+    
+}
+
+@Composable
+private fun RevealRoleComponent(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun RevealRoleComponentPreview() {
+    ByteClubTheme {
+        RevealRoleComponent()
+    }
+}
+
+@Preview
+@Composable
+private fun HiddenRoleComponentPreview() {
+    ByteClubTheme {
+        HiddenRoleComponent()
+    }
 }
 
 @Preview
