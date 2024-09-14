80e7ec4f7bad980fd7556c16f299fb09d1d1b9af
Sep <sepehr.pourekrami@gmail.com>
2024-09-14 16:50:03 +0330

    secret hitler cards

---
+++ app/src/main/java/com/sep/byteClub/domain/entiry/secretHitler/SecretHitlerCardEntity.kt
@@ -0,0 +1,8 @@
+package com.sep.byteClub.domain.entiry.secretHitler
+
+import androidx.compose.ui.graphics.Color
+
+enum class SecretHitlerCardEntity(val color: Color) {
+    LIBERAL(Color(0xFF3BB7CC)),
+    FASCISM(Color(0xFF630101))
+}
--- app/src/main/java/com/sep/byteClub/ui/screen/secretHitler/board/BoardViewModel.kt
+++ app/src/main/java/com/sep/byteClub/ui/screen/secretHitler/board/BoardViewModel.kt
@@ -2,6 +2,7 @@ package com.sep.byteClub.ui.screen.secretHitler.board
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sep.byteClub.domain.entiry.secretHitler.SecretHitlerCardEntity
 import com.sep.byteClub.domain.entiry.secretHitler.SecretHitlerPlayerEntity
 import com.sep.byteClub.domain.usecase.secretHitler.SecretHitlerFetchPlayersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,8 +19,22 @@ class BoardViewModel @Inject constructor(
     private val _players = MutableStateFlow<ArrayList<SecretHitlerPlayerEntity>>(arrayListOf())
     val players = _players.asStateFlow()
 
+    val usedCards = MutableStateFlow<ArrayList<SecretHitlerCardEntity>>(arrayListOf())
+    val unUsedCards = MutableStateFlow<ArrayList<SecretHitlerCardEntity>>(arrayListOf())
+
     init {
         fetchPlayers()
+        initCardsToPlay()
+    }
+
+    private fun initCardsToPlay() {
+        for (i in 0 until 13) {
+            unUsedCards.value.add(SecretHitlerCardEntity.FASCISM)
+        }
+        for (i in 0 until 7) {
+            unUsedCards.value.add(SecretHitlerCardEntity.LIBERAL)
+        }
+        unUsedCards.value.shuffle()
     }
 
     private fun fetchPlayers() {
