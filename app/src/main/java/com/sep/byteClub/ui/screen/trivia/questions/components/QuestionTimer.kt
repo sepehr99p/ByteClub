21049b09ce2c889fd57f4b33a4b62d91654268b6
Sep <sepehr.pourekrami@gmail.com>
2024-12-26 17:47:00 +0100

    question timer to be progress indicator

--- app/src/main/java/com/sep/byteClub/ui/screen/trivia/questions/components/QuestionTimer.kt
+++ app/src/main/java/com/sep/byteClub/ui/screen/trivia/questions/components/QuestionTimer.kt
@@ -1,8 +1,10 @@
 package com.sep.byteClub.ui.screen.trivia.questions.components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -23,41 +25,39 @@ enum class TimerState {
 @Composable
 internal fun QuestionTimer(
     modifier: Modifier = Modifier,
-    timeRequired: Int = 20,
     timerState: MutableState<TimerState>,
     onTimerFinished: () -> Unit
 ) {
-    val sliderValue = remember {
-        mutableFloatStateOf(0f)
-    }
-    val eachStep = ((100f) / timeRequired) / 100f
+    val maxTime = 30f
+    var progress = remember { mutableFloatStateOf(0f) }
+    val animatedProgress = animateFloatAsState(
+        targetValue = (progress.floatValue / (maxTime)).toFloat(),
+        animationSpec = tween(durationMillis = 1000, easing = LinearEasing),
+        label = ""
+    )
 
     LaunchedEffect(timerState.value) {
-        if (timerState.value == TimerState.RESTART) {
-            sliderValue.floatValue = 0f
-        }
-        if (timerState.value == TimerState.PAUSE) {
-            onTimerFinished.invoke()
-        } else {
-            while (sliderValue.floatValue != 1f) {
-                delay(Delay)
-                sliderValue.floatValue += eachStep
-                if (sliderValue.floatValue >= 1f) {
-                    onTimerFinished.invoke()
+        when (timerState.value) {
+            TimerState.PAUSE -> onTimerFinished.invoke()
+            else -> {
+                if (timerState.value == TimerState.RESTART) {
+                    progress.floatValue = 0f
+                }
+                while (progress.floatValue != maxTime) {
+                    delay(Delay)
+                    progress.floatValue += 1f
+                    if (progress.floatValue >= maxTime) {
+                        onTimerFinished.invoke()
+                    }
                 }
             }
         }
     }
-    Slider(
+
+    LinearProgressIndicator(
         modifier = modifier.fillMaxWidth(),
-        value = sliderValue.floatValue,
-        onValueChange = {
-            sliderValue.floatValue = it
-        },
-        enabled = false,
-        colors = SliderDefaults.colors(
-            thumbColor = Color.Transparent, disabledThumbColor = Color.Transparent
-        )
+        progress = { animatedProgress.value },
+        color = Color.Red
     )
 }
 
