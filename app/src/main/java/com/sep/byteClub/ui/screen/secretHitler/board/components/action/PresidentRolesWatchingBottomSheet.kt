c0d8dac0961bee467b66ba8e341118216f6271e4
Sep <sepehr.pourekrami@gmail.com>
2024-12-26 16:26:31 +0100

    ui update on president role watching

--- app/src/main/java/com/sep/byteClub/ui/screen/secretHitler/board/components/action/PresidentRolesWatchingBottomSheet.kt
+++ app/src/main/java/com/sep/byteClub/ui/screen/secretHitler/board/components/action/PresidentRolesWatchingBottomSheet.kt
@@ -1,6 +1,7 @@
 package com.sep.byteClub.ui.screen.secretHitler.board.components.action
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sep.byteClub.domain.entiry.secretHitler.SecretHitlerPlayerEntity
@@ -33,6 +35,9 @@ import com.sep.byteClub.ui.designSystem.theme.Regular_12
 import com.sep.byteClub.ui.designSystem.theme.dimen.corner_24
 import com.sep.byteClub.ui.designSystem.theme.dimen.padding_16
 import com.sep.byteClub.ui.designSystem.theme.dimen.padding_8
+import com.sep.byteClub.R
+import com.sep.byteClub.ui.designSystem.theme.dimen.border_1
+import com.sep.byteClub.ui.designSystem.theme.dimen.corner_16
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,13 +64,29 @@ internal fun PresidentRolesWatching(
             colors = CardDefaults.elevatedCardColors(containerColor = MaterialTheme.colorScheme.surfaceContainer),
         ) {
             val seen = remember { mutableStateOf(false) }
-            LazyColumn {
+            LazyColumn(
+                modifier = Modifier.padding(padding_8),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    PresidentRolesWatchingHeaderItem()
+                }
                 items(players) { player ->
                     PresidentRolesWatchingItems(modifier = Modifier, player, seen)
                 }
             }
         }
     }
+}
+
+@Composable
+private fun PresidentRolesWatchingHeaderItem(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier.padding(vertical = padding_8),
+        text = stringResource(R.string.select_player),
+        style = Bold_12,
+        color = MaterialTheme.colorScheme.onPrimary
+    )
 }
 
 @Composable
@@ -79,7 +100,15 @@ private fun PresidentRolesWatchingItems(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(padding_16)
+            .padding(padding_8)
+            .border(
+                width = border_1,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                shape = RoundedCornerShape(
+                    corner_16
+                )
+            )
+            .padding(padding_8)
             .clickable(interactionSource = interactionSource, indication = null) {
                 if (seen.value.not()) {
                     showRole.value = true
