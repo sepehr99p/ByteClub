6f00015cec132ca657c1bea83225994cf8ab3a75
Sep <sepehr.pourekrami@gmail.com>
2024-12-30 12:47:58 +0100

    f1 domain usecase impl

---
+++ app/src/main/java/com/sep/byteClub/domain/usecase/f1/FetchF1IntervalsUseCase.kt
@@ -0,0 +1,10 @@
+package com.sep.byteClub.domain.usecase.f1
+
+import com.sep.byteClub.domain.repository.f1.F1Repository
+import javax.inject.Inject
+
+class FetchF1IntervalsUseCase @Inject constructor(
+    private val f1Repository: F1Repository
+) {
+    suspend operator fun invoke() = f1Repository.fetchIntervals()
+}
