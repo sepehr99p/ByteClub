b4d535c43da46dc320af93e0f660e0356d100ffb
Sep <sepehr.pourekrami@gmail.com>
2024-12-30 16:03:42 +0100

    home screen for f1 minor update

--- app/src/main/java/com/sep/byteClub/ui/screen/f1/F1HomeScreen.kt
+++ app/src/main/java/com/sep/byteClub/ui/screen/f1/F1HomeScreen.kt
@@ -1,12 +1,25 @@
 package com.sep.byteClub.ui.screen.f1
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sep.byteClub.ui.designSystem.theme.Bold_20
 
 @Composable
 fun F1HomeScreen(modifier: Modifier = Modifier) {
-
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "coming soon", color = MaterialTheme.colorScheme.onPrimary, style = Bold_20)
+    }
 }
 
 @Preview
